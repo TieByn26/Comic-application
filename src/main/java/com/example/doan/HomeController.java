d59b37b47f37954ef0b0269248547b01f9b5c0c0
Vuong Ngoc Thuan <vanthuan678310@gmail.com>
2024-05-21 00:31:42 +0700

    upload code homeview #1

---
+++ src/main/java/com/example/doan/HomeController.java
@@ -0,0 +1,24 @@
+package com.example.doan;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class HomeController extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader loader = new FXMLLoader(HomeController.class.getResource("/com/example/doan/home.fxml"));
+
+        Scene scene = new Scene(loader.load());
+        stage.setHeight(800);
+        stage.setWidth(1200);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
---
+++ src/main/java/com/example/doan/signInController.java
@@ -0,0 +1,4 @@
+package com.example.doan;
+
+public class signInController {
+}
