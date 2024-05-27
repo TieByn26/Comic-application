b3d2cde2aa6a954e7c5072277d43ad8d98d5238d
Vuong Ngoc Thuan <vanthuan678310@gmail.com>
2024-05-27 12:12:04 +0700

    conpleted view homme

--- src/main/java/com/example/doan/HomeController.java
+++ src/main/java/com/example/doan/HomeController.java
@@ -1,24 +1,83 @@
 package com.example.doan;
 
-import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
+import javafx.geometry.Insets;
+import javafx.scene.Parent;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.TilePane;
+import javafx.scene.layout.VBox;
 
-import java.io.IOException;
+public class HomeController {
+    private String pathPaneComics = "/com/example/doan/paneComics.fxml";
+    private String pathTopUser = "/com/example/doan/paneTopUser.fxml";
 
-public class HomeController extends Application {
-    @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader loader = new FXMLLoader(HomeController.class.getResource("/com/example/doan/home.fxml"));
+    @FXML
+    private ScrollPane home_scroller;
 
-        Scene scene = new Scene(loader.load());
-        stage.setHeight(800);
-        stage.setWidth(1200);
-        stage.show();
+    @FXML
+    private TilePane home_listComics;
+
+    @FXML
+    private VBox home_parentListComics;
+
+    @FXML
+    private HBox home_listTopComics;
+
+    @FXML
+    private VBox home_listTopUser;
+
+    @FXML
+    private  ScrollPane home_scrollListTopUser;
+
+    public void initialize() throws Exception {
+        //call function upload list comics to screen
+        uploadListComics();
+
+        //call function upload list top comics to screen
+        uploadListTopComics();
+
+        //call function upload list top user to screen
+        uploadTopUser();
+
     }
 
-    public static void main(String[] args) {
-        launch();
+    private void uploadListComics() throws Exception{
+        // Thiết lập TilePane
+        home_listComics.setPadding(new Insets(10));
+        home_listComics.setHgap(10);
+        home_listComics.setVgap(10);
+
+        // Thêm các phần tử vào TilePane
+        for (int i = 0; i < 30; i++) {
+            FXMLLoader newComicsLoader = new FXMLLoader(getClass().getResource(pathPaneComics));
+            Parent comicPane = newComicsLoader.load();
+            home_listComics.getChildren().add(comicPane);
+        }
+
+        // Thiết lập VBox để căn giữa TilePane
+        home_parentListComics.setPadding(new Insets(10));
+    }
+
+    private void uploadListTopComics() throws  Exception {
+        for(int i = 0; i < 30; i++) {
+            FXMLLoader newTopComics = new FXMLLoader(getClass().getResource(pathPaneComics));
+            Parent rootNewTopComics = newTopComics.load();
+
+            home_listTopComics.getChildren().add(rootNewTopComics);
+        }
+        home_listTopComics.setSpacing(10);
+    }
+
+    private void uploadTopUser() throws Exception {
+        for (int i = 0; i < 20; i++) {
+            FXMLLoader newTopUser = new FXMLLoader(getClass().getResource(pathTopUser));
+            Parent rootNewTopUser = newTopUser.load();
+
+            home_listTopUser.getChildren().add(rootNewTopUser);
+        }
+        home_listTopUser.setSpacing(10);
     }
 }
