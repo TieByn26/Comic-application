fa8fa5877892945b91b4ffcd378396cde80d184d
Vuong Ngoc Thuan <vanthuan678310@gmail.com>
2024-05-29 08:34:28 +0700

    stash pop nham nhanh

---
+++ src/main/java/ObjectGson/GsonForServer/SV_History.java
@@ -0,0 +1,25 @@
+package ObjectGson.GsonForServer;
+
+public class SV_History {
+    private int idUser;
+    private String idComic;
+    private int chapter;
+
+    public SV_History() {
+    }
+
+    public SV_History(int idUser, String idComic, int chapter) {
+        this.idUser = idUser;
+        this.idComic = idComic;
+        this.chapter = chapter;
+    }
+
+    @Override
+    public String toString() {
+        return "SV_History{" +
+                "idUser=" + idUser +
+                ", idComic='" + idComic + '\'' +
+                ", chapter=" + chapter +
+                '}';
+    }
+}
